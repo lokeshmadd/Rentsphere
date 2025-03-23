25a27c03b4d47e0341209c4ffb218ff27045ccf7
Lokesh Maddipati <lokeshcu48@gmail.com>
2025-03-23 14:44:23 +0530

    Add files via upload

--- models/cart.js
+++ models/cart.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
 
+// Schema for items inside the cart
+const cartItemSchema = new mongoose.Schema({
+  itemId: { type: mongoose.Schema.Types.ObjectId, ref: 'Ad', required: true },
+  quantity: { type: Number, default: 1 }
+});
+
+// Cart schema
+const cartSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false }, // Will add multi-user later
+  items: [cartItemSchema]
+});
+
+// Exporting the model
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
